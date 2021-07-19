663df60b65685ec984a2284321af9714d3016baa
leandercibi <36109811+leandercibi@users.noreply.github.com>
2021-07-20 03:26:33 +0530

    changes

---
+++ oconfig.js
@@ -0,0 +1,11 @@
+const config = {
+    client: {
+      id: '<client-id>',
+      secret: '<client-secret>'
+    },
+    auth: {
+      tokenHost: 'https://api.oauth.com'
+    }
+  };
+   
+  const { ClientCredentials, ResourceOwnerPassword, AuthorizationCode } = require('simple-oauth2');
